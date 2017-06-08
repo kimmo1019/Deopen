6cd9fcfdd7c075b95f98d5ab3c3774fef2b2fcb7
Qiao Liu <liuqiao@buaa.edu.cn>
2017-06-08 14:45:49 +0800

    Add files via upload

---
+++ Deopen_classification.py
@@ -0,0 +1,116 @@
+'''
+This script is used for running Deopen model.
+Usage:
+    python Deopen_classification.py -in /path/to/preprocessed_file -out /path/to/outputfile
+'''
+import hickle as hkl
+import argparse
+from sklearn.cross_validation import ShuffleSplit
+import numpy as np
+import hickle as hkl
+import theano
+from lasagne.layers import InputLayer
+from lasagne.layers import DenseLayer
+from lasagne.layers import DropoutLayer
+from lasagne.layers import SliceLayer
+from lasagne.layers import FlattenLayer
+from lasagne.layers import ConcatLayer
+try:
+    from lasagne.layers.dnn import Conv2DDNNLayer as Conv2DLayer
+    from lasagne.layers.dnn import MaxPool2DDNNLayer as MaxPool2DLayer
+    print 'Using Lasagne.layers.dnn (faster)'
+except ImportError:
+    from lasagne.layers import Conv2DLayer
+    from lasagne.layers import MaxPool2DLayer
+    print 'Using Lasagne.layers (slower)'
+from lasagne.nonlinearities import softmax
+from lasagne.updates import adam
+from nolearn.lasagne import NeuralNet
+from nolearn.lasagne import TrainSplit
+from sklearn import metrics
+floatX = theano.config.floatX
+
+
+
+def data_split(inputfile):
+    data = hkl.load(inputfile)
+    X = data['mat']
+    X_kspec = data['kmer']
+    y = data['y']
+    rs = ShuffleSplit(len(y), n_iter=1,random_state = 1)
+    X_kspec = X_kspec.reshape((X_kspec.shape[0],1024,4))
+    X = np.concatenate((X,X_kspec), axis = 1)
+    X = X[:,np.newaxis]
+    X = X.transpose((0,1,3,2))
+    for train_idx, test_idx in rs:
+        X_train = X[train_idx,:]
+        y_train = y[train_idx]
+        X_test = X[test_idx,:]
+        y_test = y[test_idx]
+    X_train = X_train.astype('float32')
+    y_train = y_train.astype('int32')
+    X_test = X_test.astype('float32')
+    y_test = y_test.astype('int32')
+    return [X_train, y_train, X_test, y_test]
+
+def model_train(X_train, y_train,learning_rate = 1e-4,epochs = 10):
+    l = 1000
+    layer1 = InputLayer(shape=(None, 1, 4, l+1024))
+    layer2_1 = SliceLayer(layer1, indices=slice(0, l), axis = -1)
+    layer2_2 = SliceLayer(layer1, indices=slice(l, None), axis = -1)
+    layer2_3 = SliceLayer(layer2_2, indices = slice(0,4), axis = -2)
+    layer2_f = FlattenLayer(layer2_3)
+    layer3 = Conv2DLayer(layer2_1,num_filters = 64, filter_size = (4,7))
+    layer4 = Conv2DLayer(layer3,num_filters = 64, filter_size = (1,7))
+    layer5 = Conv2DLayer(layer4,num_filters = 64, filter_size = (1,7))
+    layer6 = MaxPool2DLayer(layer5, pool_size = (1,6))
+    layer7 = Conv2DLayer(layer6,num_filters = 64, filter_size = (1,7))
+    layer8 = Conv2DLayer(layer7,num_filters = 64, filter_size = (1,7))
+    layer9 = Conv2DLayer(layer8,num_filters = 64, filter_size = (1,7))
+    layer10 = MaxPool2DLayer(layer9, pool_size = (1,6))
+    layer11 = Conv2DLayer(layer10,num_filters = 64, filter_size = (1,7))
+    layer12 = Conv2DLayer(layer11,num_filters = 64, filter_size = (1,7))
+    layer13 = Conv2DLayer(layer12,num_filters = 64, filter_size = (1,7))
+    layer14 = MaxPool2DLayer(layer13, pool_size = (1,6))
+    layer14_d = DenseLayer(layer14, num_units= 64)
+    layer3_2 = DenseLayer(layer2_f, num_units = 64)
+    layer15 = ConcatLayer([layer14_d,layer3_2])
+    #layer15 = ConcatLayer([layer10_d,])
+    layer16 = DropoutLayer(layer15)
+    layer17 = DenseLayer(layer16, num_units=32)
+    network = DenseLayer(layer17, num_units= 2, nonlinearity=softmax)
+    lr = theano.shared(np.float32(learning_rate))
+    net = NeuralNet(
+                network,
+                max_epochs=epochs,
+                update=adam,
+                update_learning_rate=lr,
+                train_split=TrainSplit(eval_size=0.1),
+                #on_epoch_finished=[AdjustVariable(lr, target=1e-8, half_life=20)],
+                verbose=5)
+    net.fit(X_train, y_train)
+    return net
+
+def model_test(net, X_test, y_test):
+    #net.load_params_from('/path/to/weights_file')
+    y_pred = net.predict(X_test)
+    y_prob = net.predict_proba(X_test)
+    print 'Accuracy score is {}'.format(metrics.accuracy_score(y_test, y_pred))
+    print 'ROC AUC score is {}'.format(metrics.roc_auc_score(y_test, y_prob[:,-1]))
+    #hkl.dump([y_prob[:,-1],y_test],'/path/to/prediction_outcome')
+
+def save_model(model,outputfile):
+    net.save_params_to(open(outputfile,'w'))
+
+
+if  __name__ == "__main__" :
+    parser = argparse.ArgumentParser(description='Deopen classication model') 
+    parser.add_argument('-in', dest='input', type=str, help='file of preprocessed data')
+    parser.add_argument('-out', dest='output', type=str, help='output file')
+    args = parser.parse_args()
+    X_train, y_train, X_test, y_test = data_split(args.input)
+    model = model_train(X_train, y_train)
+    #save_model(model, args.output)
+    model_test(model, X_test, y_test)
+
+
---
+++ Gen_data.py
@@ -0,0 +1,123 @@
+'''
+This script is used for generating dataset for Deopen.
+Usage:
+    python Gen_data.py -l 1000 -s 100000 -in /path/to/inputfile -out /path/to/outputfile
+'''
+import numpy as np
+from pyfasta import Fasta
+import hickle as hkl
+import argparse
+import gzip
+
+'''
+Input:  Sequence
+Output: One-hot encoding matrix
+'''
+def seq_to_mat(seq):
+    encoding_matrix = {'a':0, 'A':0, 'c':1, 'C':1, 'g':2, 'G':2, 't':3, 'T':3, 'n':4, 'N':4}
+    mat = np.zeros((len(seq),5))  
+    for i in range(len(seq)):
+        mat[i,encoding_matrix[seq[i]]] = 1
+    mat = mat[:,:4]
+    return mat
+
+'''
+Input:  Sequence
+Output: K-mer vector
+Default:  K=6
+'''
+def seq_to_kspec(seq, K=6):
+    encoding_matrix = {'a':0, 'A':0, 'c':1, 'C':1, 'g':2, 'G':2, 't':3, 'T':3, 'n':0, 'N':0}
+    kspec_vec = np.zeros((4**K,1))
+    for i in range(len(seq)-K+1):
+        sub_seq = seq[i:(i+K)]
+        index = 0
+        for j in range(K):
+            index += encoding_matrix[sub_seq[j]]*(4**(K-j-1))
+        kspec_vec[index] += 1
+    return kspec_vec
+
+'''
+Input: Spot, genome,label
+Output: Features of a sample including spot, sequence, one-hot encoding matrix, K-mer vector and label
+'''
+def get_all_feats(spot,genome,label):
+    ret = {}
+    ret['spot'] = spot
+    ret['seq'] = genome[spot[0]][spot[1]:spot[2]]
+    ret['mat'] = seq_to_mat(ret['seq'])
+    ret['kmer'] = seq_to_kspec(ret['seq'])
+    ret['y'] = label
+    return ret
+
+'''
+Input: Orignal dataset
+Output: save the dataset in .hkl format 
+'''
+def  save_dataset(origin_dataset,save_dir):
+    dataset = {}
+    for key in origin_dataset[0].keys():
+        dataset[key] = [item[key] for item in origin_dataset]
+    dataset['seq'] = [item.encode('ascii','ignore') for item in dataset['seq']]
+    for key in origin_dataset[0].keys():
+        dataset[key] = np.array(dataset[key])
+    print "Start to save dataset ..."
+    hkl.dump(dataset,save_dir)    
+    print 'Dataset generation is finished!'    
+
+
+'''
+Input: Raw file
+Output: generate dataset record
+'''
+def  generate_records(input_file,sample_length,dataset_size,ratio = 1):
+    chrom_iter = 0 
+    dataset=[]
+    chrom_dict=[str(item) for item in range(1,23)]+['X','Y']
+    DATA_PATH = './openness_pre/data'
+    genome = Fasta(DATA_PATH+'/genome.fa')
+    chromosome_len_file=DATA_PATH+'/chromosome_len.txt'
+    with open(chromosome_len_file,'r') as f:
+        chrom_lens = f.readlines() 
+    openness_records=[]
+    with gzip.open(input_file,'r') as f:
+        for line in f:
+            line = line.split()
+            openness_records.append([line[0], int(line[1]), int(line[2]), int(line[4]), float(line[5])/float(line[4])])  
+
+    while(len(dataset)<dataset_size):
+        chrom = 'chr'+chrom_dict[chrom_iter]
+        records_in_chrom = [item for item in openness_records if item[0]==chrom]        
+        samples_pos = [[item[0], (item[1]+item[2])/2 - sample_length/2, (item[1]+item[2])/2 + sample_length/2] for item in records_in_chrom]        
+        for each in samples_pos:
+            dataset.append(get_all_feats(each,genome,1))
+        chrom_len = int(chrom_lens[chrom_iter].strip().split()[-1])
+        obin = np.zeros((chrom_len,1))
+        for item in records_in_chrom:
+            obin[item[1]:item[2]] = 1
+        samples_neg = []
+        while len(samples_neg) < int(len(records_in_chrom)*ratio):
+            middle = np.random.randint(chrom_len)
+            start = middle - sample_length/2
+            end = middle + sample_length/2
+            if np.sum(obin[start:end]) == 0     \
+                    and  'N' not in genome[chrom][start:end]    \
+                    and  'n' not in genome[chrom][start : end] :
+                samples_neg.append([chrom,start,end])
+        for each in samples_neg:
+            dataset.append(get_all_feats(each,genome,0))
+        chrom_iter +=1
+    return  dataset
+        
+        
+if  __name__ == "__main__" : 
+    parser = argparse.ArgumentParser(description='Deopen data generation') 
+    parser.add_argument('-l', dest='length', type=int, default=1000, help='sequence length')
+    parser.add_argument('-s', dest='size', type=int, default=100000, help='number of samples')
+    parser.add_argument('-in', dest='input', type=str, help='file of raw input data')
+    parser.add_argument('-out', dest='output', type=str, help='output file')
+    args = parser.parse_args()   
+    dataset = generate_records(args.input,args.length,args.size)
+    save_dataset(dataset,args.out)
+ 
+    
